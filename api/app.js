58b549ba5afefabec3f20d3ffd4398eb1e4837d4
Nicholas Mariga <dolphnikoz@gmail.com>
2021-12-24 17:36:45 +0300

    second module

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const router = require('./src/router');
 
 const app = express();
 
@@ -9,6 +10,20 @@ Right above our module.exports line, let's define a constant pathToIndex to repr
  Let's pass __dirname as the first argument, and a string value of '../client/index.html' as the second argument */
 
 const pathToIndex = path.resolve(__dirname, "../client/index.html");
+
+/* Wire up the router
+
+In ./api/app.js, let's go ahead and require our router from ./src/router. Before our previous app.use(), 
+let's call the use() method again on our app object. Pass it the route '/' as the first argument and our
+ router object as the second. */
+app.use('/',router);
+
+/* Serve static files
+
+Let's call app.use() again, passing it express.static() as its only argument. 
+Pass to express.static() a call to path.resolve(). 
+You should call path.resolve() with the arguments __dirname and 'uploads', in that order. */
+app.use(express.static(path.resolve(__dirname, 'uploads')));
 
 /* Let's call the use() method of our app. For the route argument, 
 let's pass a string of '/*'. For the route handler, we will pass an anonymous function that takes two parameters,
---
+++ api/src/router.js
@@ -0,0 +1,84 @@
+const { Router } = require("express");
+const multer = require("multer");
+
+const router = Router();
+
+/* Write the filename handler function
+
+Let's write a function called filename. It should take request, file, and callback as its parameters. 
+Inside the function body make a call to callback() passing in null as the first argument and 
+file.originalname as the second argument. */
+
+/* function filename (request, file, callback){
+    callback(null, file.originalname);
+} */
+const filename = (request, file, callback) => {
+  callback(null, file.originalname);
+};
+/* 
+Configure Multer diskStorage
+
+Next after the express require, let's require multer. Declare a constant called storage. 
+Assign to it a call to the diskStorage() method of multer. Pass an object literal as the only argument. 
+The object literal should have two properties: the first with a key of destination and a value of 'api/uploads/';
+ the second with a key of filename and a value of filename. 
+ Note: the 2nd property is referring to the filename function */
+const storage = multer.diskStorage({
+  destination: "api/uploads/",
+  filename: filename,
+});
+/* Create the MIME type file filter
+
+Declare a function called fileFilter. It should take request, file, and callback as parameters. 
+Inside the function body declare an if statement that runs its code block if file.mimetype does 
+not strictly equal 'image/png'. Inside the if block, let's assign to request.fileValidationError 
+the string 'Wrong file type'. Then, make a call to the callback() parameter, passing in null, 
+false, and a new Error object with the message 'Wrong file type'. 
+Next, in an else block, let's call callback(), passing in null as the first argument, and true as the second. */
+const fileFilter = (request, file, callback) => {
+  if (file.mimetype !== "image/png") {
+    request.fileValidatorError = "Wrong file type";
+    callback(null, false, new Error("Wrong file type"));
+  } else {
+    callback(null, true);
+  }
+};
+
+/* Define the upload callback
+
+Declare a constant named upload. Assign to upload a call to multer(), passing in an object literal. 
+This object literal will have two properties. It's first property will be the key fileFilter which 
+refers to our fileFilter() function. Its second property has a key called storage, and whose value
+ is our storage constant. */
+const upload = multer({
+  fileFilter,
+  storage,
+});
+
+/* Create the upload route
+
+Back in router.js, call the post() method of our router object. 
+Let's pass the route '/upload' as its first argument. 
+The second argument should be a call to the upload object's method single(), 
+passing in the string 'photo'. The third argument is an anonymous function 
+that takes request and response as parameters. Inside the function body, */
+router.post(
+  "/upload",
+  upload.single("photo", (request, response) => {
+    /* check if the request object has a fileValidationError property. 
+    If it does return a call to response.status(), passing in 400 as the lone argument. 
+    Chain a call to json(), passing in an object literal with a key of error and
+     a value of request.fileValidationError. */
+    if (request.fileValidatorError) {
+      return response.status(400).json({ error: request.fileValidatorError });
+    }
+    /* Respond with a 201
+
+            If there is no fileValidationError on request, let's return a call to response.status(), passing in 201. 
+            Let's chain a call to json(), passing in an object literal with a key of success and 
+            the boolean value true. */
+    return response.status(201).json({ success: true });
+  })
+);
+
+module.exports = router;
